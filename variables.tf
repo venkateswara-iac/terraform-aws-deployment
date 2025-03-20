491f2bfdbe48d979e0098ab6b58970845ab7ea47
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-03-20 12:14:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
