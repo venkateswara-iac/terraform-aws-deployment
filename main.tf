8e860e5baf54cd85fbfe6598996eb6e02926217e
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-03-20 15:22:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your AWS region
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (Update this)
+resource "aws_instance" "ven_gitaction_ec2" {
+  ami           = "ami-00bb6a80f01f03502"  # Ubuntu Server 24.04 LTS
   instance_type = "t2.micro"
+  key_name      = "VENSNR01"  # Replace with your actual key pair name
+  subnet_id     = "subnet-0df27e373d7a852bc"  # Replace with your actual subnet ID
+  vpc_security_group_ids = ["sg-0e5cf05ab17abc5cf"]  # Replace with your security group ID
 
   tags = {
-    Name = "GitHubActionsEC2"
+    Name = "GitHub-Actions-EC2"
   }
 }
