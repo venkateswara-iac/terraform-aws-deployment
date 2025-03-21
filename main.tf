86e18e58a8b675bd83eaf1a68cec7abc84716bc3
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-03-21 11:06:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ven_gitaction_ec2" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   key_name      = "VENSNR01"
   subnet_id     = "subnet-0df27e373d7a852bc"
   vpc_security_group_ids = ["sg-0e5cf05ab17abc5cf"]
